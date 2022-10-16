f618598843e4cb36d77b426037bc32e049457673
DreamAlone <2480138765@qq.com>
2022-10-16 13:39:07 +0800

    Feature: Add system support for Scene Feature: Add SystemGroup to help group systems

--- pyecs.py
+++ pyecs.py
@@ -1,9 +1,12 @@
-from typing import Any, Iterable, Type, TypeVar
+from typing import Any, Callable, Iterable, Optional, Sequence, Tuple, Type, TypeVar
 from collections import defaultdict
 from itertools import count
+from functools import reduce
+from operator import and_, methodcaller
 
 __all__ = [
-    'Scene'
+    'Scene',
+    'SystemGroup'
 ]
 
 T = TypeVar('T')
@@ -13,6 +16,7 @@ class Scene:
     
     def __init__(self):
         self.data:defaultdict[type,dict[int,Any]] = defaultdict(dict)
+        self.systems:list[Callable] = []
     
     def add_entity(self, *components) -> int:
         ent = next(self._entity_creator)
@@ -25,13 +29,66 @@ class Scene:
         self.data[type(component)][entity] = component
     
     def destroy_entity(self, entity:int):
-        """Try destroying an entity with its components whether it exist or not."""
+        """Try destroying an entity with its components whether it exists or not."""
         for objs in self.data.values():
             objs.pop(entity, None)
     
     def destroy_component(self, entity:int, component):
-        """Try destroying a component whether it exist or not."""
+        """Try destroying a component whether it exists or not."""
         self.data[type(component)].pop(entity)
     
     def get_components(self, type_:Type[T]) -> Iterable[T]:
         return tuple(self.data[type_].values())
+    
+    def add_system(self, system:'Callable[[Scene],Any]'):
+        """Noted that `system` should recive the `Scene` it is in as the first parameter."""
+        self.systems.append(system)
+
+    def destroy_system(self, system:Callable):
+        """Try destroying a system whether it exists or not."""
+        try:
+            self.systems.remove(system)
+        except ValueError:
+            return
+    
+    def tick(self):
+        """Call all the systems."""
+        for system in self.systems.copy():
+            system(self)
+
+class SystemGroup:
+
+    def __init__(self):
+        self.systems:defaultdict[tuple[type],list[Callable]] = defaultdict(list)
+
+    def add(self, system:Callable, types:Optional[Sequence[type]]=None):
+        """
+        Add a `system` to the group.
+        
+        If `types` is kept `None`, the annotations of `system` will be used.
+        """
+        if types is None:
+            types = self._get_types(system)
+        self.systems[tuple(types)].append(system)
+        return system
+    
+    def destroy(self, system:Callable):
+        """Try destroying a system whether it exists or not."""
+        for systems in self.systems.values():
+            try:
+                systems.remove(system)
+                return
+            except ValueError:
+                continue
+
+    def _get_types(self, system:Callable) -> Tuple[type]:
+        return getattr(system, '__annotations__')['comps'].__args__
+    
+    def __call__(self, scene:Scene):
+        for types, systems in self.systems.items():
+            # Get entities common to types
+            comp_dicts:tuple[dict[int,Any]] = tuple(map(scene.data.get, types))
+            entities:set[int] = reduce(and_, map(dict.keys, comp_dicts))  # type: ignore
+            comps = tuple(map(methodcaller('get', entity), comp_dicts) for entity in entities)
+            for system in systems:
+                system(*comps)
--- tests/test_ecs.py
+++ tests/test_ecs.py
@@ -1,4 +1,5 @@
-from pyecs import Scene
+from typing import Tuple
+from pyecs import Scene, SystemGroup
 
 class Type1:
     pass
@@ -22,3 +23,48 @@ def test_Scene():
 
     scene.destroy_entity(ent)
     assert comp1 not in scene.get_components(Type1)
+
+def test_Scene_tick():
+    scene = Scene()
+    d = {'value': 'before_tick'}
+    def func(scene):
+        d['value'] = 'after_tick'
+    
+    scene.add_system(func)
+    scene.tick()
+    assert d['value'] == 'after_tick'
+
+def test_SystemGroup():
+    sysgroup = SystemGroup()
+    def func1(*comps:Tuple[Type1,Type2]):
+        pass
+
+    types1 = sysgroup._get_types(func1)
+    assert (Type1, Type2) == types1
+
+    sysgroup.add(func1, types1)
+    assert func1 in sysgroup.systems[types1]
+
+    @sysgroup.add
+    def func2(*comps:Tuple[Type2,Type1]):
+        pass
+    assert func2 in sysgroup.systems[sysgroup._get_types(func2)]
+
+    sysgroup.destroy(func1)
+    assert func1 not in sysgroup.systems[types1]
+
+def test_SystemGroup_tick():
+    scene = Scene()
+    ent = scene.add_entity(Type2(), Type1())
+    sysgroup = SystemGroup()
+    d = {'value': 'before_tick'}
+
+    @sysgroup.add
+    def func(*comps:Tuple[int,Type1,Type2]):
+        for entity, type1, type2 in comps:
+            assert entity == ent
+            assert type(type1) is Type1
+            assert type(type2) is Type2
+            d['value'] = 'after_tick'
+    sysgroup(scene)
+    assert d['value'] == 'after_tick'
