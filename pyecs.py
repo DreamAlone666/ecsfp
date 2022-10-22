1953408d1e4b1bf5466d0a3ada6d3413ace29bb2
DreamAlone <2480138765@qq.com>
2022-10-22 19:22:49 +0800

    Style: PEP8 type hints

--- pyecs.py
+++ pyecs.py
@@ -1,12 +1,13 @@
-from typing import Any, Callable, Iterable, Optional, Sequence, Tuple, Type, TypeVar
 from collections import defaultdict
-from itertools import count
 from functools import reduce
+from itertools import count
 from operator import and_, methodcaller
+from typing import (Any, Callable, DefaultDict, Dict, Iterable, List, Optional,
+                    Sequence, Set, Tuple, Type, TypeVar)
 
 __all__ = [
     'Scene',
-    'SystemGroup'
+    'SystemGroup',
 ]
 
 T = TypeVar('T')
@@ -15,8 +16,8 @@ class Scene:
     _entity_creator = count()
     
     def __init__(self):
-        self.data:defaultdict[type,dict[int,Any]] = defaultdict(dict)
-        self.systems:list[Callable] = []
+        self.data: DefaultDict[type, Dict[int, Any]] = defaultdict(dict)
+        self.systems: List[System] = []
     
     def add_entity(self, *components) -> int:
         ent = next(self._entity_creator)
@@ -25,32 +26,32 @@ class Scene:
             self.add_component(ent, comp)
         return ent
     
-    def add_component(self, entity:int, component):
+    def add_component(self, entity: int, component):
         self.data[type(component)][entity] = component
     
-    def destroy_entity(self, entity:int):
+    def destroy_entity(self, entity: int):
         """Try destroying an entity with its components whether it exists or not."""
         for objs in self.data.values():
             objs.pop(entity, None)
     
-    def destroy_component(self, entity:int, component):
+    def destroy_component(self, entity: int, component):
         """Try destroying a component whether it exists or not."""
         self.data[type(component)].pop(entity)
     
-    def get_components(self, type_:Type[T]) -> Iterable[T]:
+    def get_components(self, type_: Type[T]) -> Iterable[T]:
         return tuple(self.data[type_].values())
     
-    def get_components_group(self, *types:type):
+    def get_components_group(self, *types: type):
         # Get entities common to types
-        comp_dicts:tuple[dict[int,Any]] = tuple(map(self.data.get, types))
-        entities:set[int] = reduce(and_, map(dict.keys, comp_dicts))  # type: ignore
-        return tuple(tuple(map(methodcaller('get', entity), comp_dicts)) for entity in entities)
+        comp_dicts: Tuple[Dict[int, Any]] = tuple(map(self.data.__getitem__, types))
+        entities: Set[int] = reduce(and_, map(dict.keys, comp_dicts))
+        return tuple(tuple(map(methodcaller('__getitem__', entity), comp_dicts)) for entity in entities)
     
-    def add_system(self, system:'Callable[[Scene],Any]'):
-        """Noted that `system` should recive the `Scene` it is in as the first parameter."""
+    def add_system(self, system: 'System'):
+        """`system` should recive the `Scene` as the first parameter."""
         self.systems.append(system)
 
-    def destroy_system(self, system:Callable):
+    def destroy_system(self, system: Callable):
         """Try destroying a system whether it exists or not."""
         try:
             self.systems.remove(system)
@@ -62,14 +63,15 @@ class Scene:
         for system in self.systems.copy():
             system(self)
 
+System = Callable[[Scene], Any]
+
 class SystemGroup:
 
     def __init__(self):
-        self.systems:defaultdict[Tuple[type,...],list[Callable[[Scene],Any]]] = defaultdict(list)
+        self.systems: DefaultDict[Tuple[type, ...], List[System]] = defaultdict(list)
 
-    def add(self, system:Callable[[Scene],Any], types:Optional[Sequence[type]]=None):
-        """
-        Add a `system` to the group.
+    def add(self, system: System, types: Optional[Sequence[type]] = None):
+        """Add a `system` to the group.
         
         If `types` is kept `None`, the annotations of `system` will be used.
         """
@@ -78,7 +80,7 @@ class SystemGroup:
         self.systems[tuple(types)].append(system)
         return system
     
-    def destroy(self, system:Callable):
+    def destroy(self, system: Callable):
         """Try destroying a system whether it exists or not."""
         for systems in self.systems.values():
             try:
@@ -87,13 +89,13 @@ class SystemGroup:
             except ValueError:
                 continue
 
-    def _get_types(self, system:Callable) -> Tuple[type,...]:
+    def _get_types(self, system: Callable) -> Tuple[type, ...]:
         try:
             return getattr(system, '__annotations__')['comps'].__args__
         except KeyError:
             return ()
     
-    def __call__(self, scene:Scene):
+    def __call__(self, scene: Scene):
         for types, systems in self.systems.items():
             comps = scene.get_components_group(*types)
             for system in systems:
